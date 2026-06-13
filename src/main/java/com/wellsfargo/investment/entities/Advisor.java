81b9339489b994d4501c2b432d0e60eff63ed283
Vansh Sharma <2024bcavansh16957@poornima.edu.in>
2026-06-13 12:26:38 +0530

    Create Advisor.java

---
+++ src/main/java/com/wellsfargo/investment/entities/Advisor.java
@@ -0,0 +1,75 @@
+package com.wellsfargo.investment.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
+import java.util.List;
+
+@Entity
+public class Advisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
+
+    public Advisor() {}
+
+    public Advisor(String firstName, String lastName, String email, List<Client> clients) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.clients = clients;
+    }
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
